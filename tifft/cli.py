904f37c63df1f5a72ae6be798d9445b5163ff205
dceoy <dnarsil+github@gmail.com>
2021-05-01 18:55:38 +0900

    Implement macd command

--- tifft/cli.py
+++ tifft/cli.py
@@ -5,23 +5,30 @@ Technical Indicators for Financial Trading
 Usage:
     tifft -h|--help
     tifft --version
-    tifft fred [--debug|--info] [--max-rows=<int>] [--start=<date>]
+    tifft history [--debug|--info] [--max-rows=<int>] [--start=<date>]
         [--end=<date>] [--csv=<path>] <symbol>...
+    tifft macd [--debug|--info] [--max-rows=<int>] [--start=<date>]
+        [--end=<date>] [--fast-ema-span=<int>] [--slow-ema-span=<int>]
+        [--macd-ema-span=<int>] [--csv=<path>] <symbol>
 
 Options:
-    -h, --help          Print help and exit
-    --version           Print version and exit
-    --debug, --info     Execute a command with debug|info messages
-    --csv=<path>        Write data with CSV into a file
-    --start=<date>      Specify the start date (e.g., 2021-01-01)
-    --end=<date>        Specify the end date
-    --max-rows=<int>    Specify the max rows to display [default: 60]
+    -h, --help              Print help and exit
+    --version               Print version and exit
+    --debug, --info         Execute a command with debug|info messages
+    --max-rows=<int>        Specify the max rows to display [default: 60]
+    --start=<date>          Specify the start date (e.g., 2021-01-01)
+    --end=<date>            Specify the end date
+    --csv=<path>            Write data with CSV into a file
+    --fast-ema-span=<int>   Specify the fast EMA span [default: 12]
+    --slow-ema-span=<int>   Specify the slow EMA span [default: 26]
+    --macd-ema-span=<int>   Specify the MACD EMA span [default: 9]
 
 Commands:
-    fred                Fetch data from FRED
+    history                 Fetch historical data from FRED
+    macd                    Fetch historical data from FRED and calculate MACD
 
 Arguments:
-    <symbol>            Data symbol
+    <symbol>                Data symbol
 """
 
 import logging
@@ -30,7 +37,7 @@ import os
 from docopt import docopt
 
 from . import __version__
-from .datareader import fetch_df_from_fred
+from .datareader import calculate_macd_from_fred_data, fetch_fred_data
 
 
 def main():
@@ -38,11 +45,20 @@ def main():
     _set_log_config(debug=args['--debug'], info=args['--info'])
     logger = logging.getLogger(__name__)
     logger.debug(f'args:{os.linesep}{args}')
-    if args['fred']:
-        fetch_df_from_fred(
+    if args['history']:
+        fetch_fred_data(
             symbols=args['<symbol>'], output_csv_path=args['--csv'],
             start_date=args['--start'], end_date=args['--end'],
             max_rows=args['--max-rows']
+        )
+    elif args['macd']:
+        calculate_macd_from_fred_data(
+            symbol=args['<symbol>'][0], output_csv_path=args['--csv'],
+            start_date=args['--start'], end_date=args['--end'],
+            max_rows=args['--max-rows'],
+            fast_ema_span=int(args['--fast-ema-span']),
+            slow_ema_span=int(args['--slow-ema-span']),
+            macd_ema_span=int(args['--macd-ema-span'])
         )
 
 
--- tifft/datareader.py
+++ tifft/datareader.py
@@ -1,21 +1,68 @@
 #!/usr/bin/env python
 
+import logging
+import os
 from pathlib import Path
 
 import pandas as pd
 import pandas_datareader.data as pdd
 
+from .macd import MacdCalculator
 
-def fetch_df_from_fred(symbols, output_csv_path=None, start_date=None,
-                       end_date=None, max_rows=None, **kwargs):
+
+def fetch_fred_data(symbols, output_csv_path=None, start_date=None,
+                    end_date=None, max_rows=None, **kwargs):
+    logger = logging.getLogger(__name__)
     pd.set_option('display.max_rows', (int(max_rows) if max_rows else None))
-    df = pdd.DataReader(
+    print('>>\tGet historical data from FRED:\t' + ', '.join(symbols))
+    df_hist = _fetch_fred_df(
+        symbols=symbols, start_date=start_date, end_date=end_date,
+        max_rows=max_rows, **kwargs
+    )
+    logger.debug(f'df_hist:{os.linesep}{df_hist}')
+    print(df_hist)
+    if output_csv_path:
+        _write_df_to_csv(df=df_hist, csv_path=output_csv_path)
+
+
+def _fetch_fred_df(symbols, start_date=None, end_date=None, max_rows=None,
+                   **kwargs):
+    return pdd.DataReader(
         name=symbols, data_source='fred', start=start_date, end=end_date,
         **kwargs
     )
-    print('>>\tGet historical data from FRED:\t' + ', '.join(symbols))
-    print(df)
+
+
+def _write_df_to_csv(df, csv_path):
+    output_csv = Path(csv_path).resolve()
+    print(f'>>\tWrite a CSV file:\t{output_csv}')
+    df.to_csv(output_csv, mode='w', header=True, sep=',')
+
+
+def calculate_macd_from_fred_data(symbol, output_csv_path=None,
+                                  start_date=None, end_date=None,
+                                  max_rows=None, fast_ema_span=12,
+                                  slow_ema_span=26, macd_ema_span=9, **kwargs):
+    logger = logging.getLogger(__name__)
+    pd.set_option('display.max_rows', (int(max_rows) if max_rows else None))
+    print(f'>>\tGet historical data from FRED:\t{symbol}')
+    df_hist = _fetch_fred_df(
+        symbols=symbol, start_date=start_date, end_date=end_date,
+        max_rows=max_rows
+    )
+    logger.debug(f'df_hist:{os.linesep}{df_hist}')
+    print(f'>>\tCalculate MACD:\t{symbol}')
+    macdc = MacdCalculator(
+        fast_ema_span=fast_ema_span, slow_ema_span=slow_ema_span,
+        macd_ema_span=macd_ema_span, **kwargs
+    )
+    df_macd = macdc.calculate_oscillator(values=df_hist.iloc[:, 0]).rename(
+        columns={
+            'value': symbol, 'macd': 'MACD', 'macd_ema': 'MACD_EMA',
+            'signal': 'SIGNAL'
+        }
+    )
+    logger.debug(f'df_macd:{os.linesep}{df_macd}')
+    print(df_macd)
     if output_csv_path:
-        output_csv = Path(output_csv_path).resolve()
-        print(f'>>\tWrite a CSV file:\t{output_csv}')
-        df.to_csv(output_csv, mode='w', header=True, sep=',')
+        _write_df_to_csv(df=df_macd, csv_path=output_csv_path)
